b968e5602c94f6ca340f1a271890bfe0d4cac95d
Sandro Huber <sandrohuber89@hotmail.com>
2019-05-19 16:36:15 +0200

    More stuff linked against.

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 ### Start development
 - `./start_development`
+
+### Build
 - `mkdir build`
 - `cd build`
 - `cmake ../`
 - `make`
+
+Currently broken, use `g++ -g -O3 main.cpp -I/usr/usr/lib/llvm-8/include/llvm `llvm-config-8 --cxxflags --ldflags --system-libs --libs all` -o bluec`
--- main.cpp
+++ main.cpp
@@ -1,21 +1,32 @@
 #include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/Optional.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Verifier.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Host.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
 
 #include <map>
 #include <memory>
 #include <iostream>
 
-using namespace llvm;
 using namespace std;
+using namespace llvm;
+using namespace llvm::sys;
 
 static LLVMContext TheContext;
 static IRBuilder<> Builder(TheContext);
@@ -23,5 +34,18 @@ static std::unique_ptr<Module> TheModule;
 static std::map<std::string, Value *> NamedValues;
 
 int main() {
+  auto L = ConstantFP::get(TheContext, APFloat(4.0));
+  auto R = ConstantFP::get(TheContext, APFloat(5.0));
+  auto f = Builder.CreateFAdd(L, R, "addtmp");
+  f->print(errs());
+
+  InitializeAllTargetInfos();
+  /*
+  InitializeAllTargets();
+  InitializeAllTargetMCs();
+  InitializeAllAsmParsers();
+  InitializeAllAsmPrinters();
+  */
+
   return 0;
 }
